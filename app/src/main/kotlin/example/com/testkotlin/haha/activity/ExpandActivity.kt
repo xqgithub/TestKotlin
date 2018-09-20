50b8f8cf39bafa6f17fe6b11883f964451af5d50
laoa <xiaoqikillua@sina.com>
2018-09-20 13:39:14 +0700

    1.BaseActivity 基类编写 2.ToastUtils 扩展工具的编写 3.ExpandActivity 扩展方法的使用

--- app/src/main/kotlin/example/com/testkotlin/haha/activity/ExpandActivity.kt
+++ app/src/main/kotlin/example/com/testkotlin/haha/activity/ExpandActivity.kt
@@ -1,9 +1,12 @@
 package example.com.testkotlin.haha.activity
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.view.View
 import example.com.testkotlin.R
+import example.com.testkotlin.haha.base.BaseActivity
 import example.com.testkotlin.haha.utils.leftMargin
+import example.com.testkotlin.haha.utils.showLongToastSafe
+import example.com.testkotlin.haha.utils.showShortToastSafe
 import example.com.testkotlin.haha.utils.toasthaha
 import kotlinx.android.synthetic.main.activity_expand.*
 
@@ -12,20 +15,49 @@ import kotlinx.android.synthetic.main.activity_expand.*
  * 扩展
  * 在实际开发时，如果对一个类某一功能并不通用，只有某些特殊场景使用时，可以使用扩展函数，从而显得灵活多变
  */
-class ExpandActivity : AppCompatActivity() {
-
-    val TAG: String = "ExpandActivity"
-
+class ExpandActivity : BaseActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_expand)
-        initView()
         toasthaha("扩展吐司")
     }
 
-    fun initView() {
+    override fun initView() {
+
+    }
+
+    override fun getLayoutId(): Int {
+        return R.layout.activity_expand
+    }
+
+    override fun initData() {
         tv_expand_leftmargin.leftMargin = 100
     }
+
+    override fun initListener() {
+        btn_expand_1.setOnClickListener(this)
+        btn_expand_2.setOnClickListener(this)
+    }
+
+    /**
+     * 点击事件
+     */
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.btn_expand_1 -> btn_expand_1()
+            R.id.btn_expand_2 -> btn_expand_2()
+            else -> showShortToastSafe("没有该按钮存在，谢谢!")
+        }
+    }
+
+    private fun btn_expand_1() {
+        showShortToastSafe("我是海贼王路飞")
+    }
+
+    private fun btn_expand_2() {
+//        showLongToastSafe("我是你爸爸，你不服气吗？")
+        showLongToastSafe(R.string.app_name)
+    }
+
 
 }
--- app/src/main/kotlin/example/com/testkotlin/haha/utils/Expand.kt
+++ app/src/main/kotlin/example/com/testkotlin/haha/utils/Expand.kt
@@ -4,11 +4,12 @@ import android.content.Context
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
-import example.com.testkotlin.haha.app.MyApp
 
 /**
  * Created by XQ on 2018/1/5.
  * 扩展属性和方法
+ * 1.扩展函数数是指在一个类上增加一种新的行为，甚至我们没有这个类代码的访问权限
+ * 2.在Java中，通常会实现很多带有static方法的工具类，而Kotlin中扩展函数的一个优势是我们不需要在调用方法的时候把整个对象当作参数传入，它表现得就像是属于这个类的一样，而且我们可以使用this关键字和调用所有public方法。
  */
 
 
@@ -16,7 +17,7 @@ import example.com.testkotlin.haha.app.MyApp
  *吐司
  */
 fun Context.toasthaha(message: String, length: Int = Toast.LENGTH_SHORT) {
-    Toast.makeText(MyApp.context!!, message, length).show()
+    Toast.makeText(this, message, length).show()
 }
 
 /**
