0c3626898d4f2675a907b2f2749d9bf6b7813d38
laoa <xiaoqikillua@sina.com>
2018-09-21 20:23:31 +0700

    Kotlin  配置Android调试工具：Stetho

--- app/src/main/kotlin/example/com/testkotlin/haha/app/MyApp.kt
+++ app/src/main/kotlin/example/com/testkotlin/haha/app/MyApp.kt
@@ -3,6 +3,7 @@ package example.com.testkotlin.haha.app
 import android.content.Context
 import android.support.multidex.MultiDex
 import android.support.multidex.MultiDexApplication
+import com.facebook.stetho.Stetho
 
 /**
  * Created by XQ on 2018/1/5.
@@ -17,6 +18,8 @@ class MyApp : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
         context = this
+        // Init Stetho
+        Stetho.initializeWithDefaults(this)
     }
 
     override fun attachBaseContext(base: Context?) {
